9953d3198cedc931a973c949fb686bf0059e6d3c
Jose RM <mojica.raul@gmail.com>
2023-11-14 10:20:55 -0600

    Created Person, Student and Teacher Classes

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :age, name
+  attr_reader :id
+
+  private
+
+  def of_age?
+    false unless of_age? >= 18
+  end
+
+  def can_use_services?
+    false unless of_age? && parent_permission == true
+  end
+end
