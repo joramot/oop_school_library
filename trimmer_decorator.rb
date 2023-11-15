5e481dca370d00fdf36cdbb0adb67d384d81b2a2
Jose RM <mojica.raul@gmail.com>
2023-11-15 09:38:02 -0600

    Fixed issue in the trimmer_decorator.rb file in the correct_name method to return correctly the name fixed the issue in the capitalize_decorator.rb file in the correct_name method for returning the name in uppercase

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,8 +2,8 @@ require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     name = @nameable.correct_name
-    return unless name.length > 10
+    return name[0..9] if name.length > 10
 
-    name[0..9]
+    name
   end
 end
