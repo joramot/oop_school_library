03ff6cf0fdc3ef84c1a1a1a161badd9cbbadd07d
Jose RM <mojica.raul@gmail.com>
2023-11-21 15:11:44 -0600

    Refactoring of the app class to separate the functionality into different classes and comply with the principle of single responsibility.

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-require_relative 'book'
-require_relative 'rental'
-require_relative 'student'
-require_relative 'teacher'
 require_relative 'classroom'
+require_relative 'display'
+require_relative 'person_creator'
+require_relative 'book_creator'
+require_relative 'rental_creator'
 
 class App
   # Defined constants for better readability for person types.
@@ -16,93 +16,36 @@ class App
   end
 
   def display_books
-    @books.each do |book|
-      puts "Title: '#{book.title}', Author: #{book.author}"
-    end
+    Display.books(@books)
   end
 
   def display_people
-    @people.each do |person|
-      puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
+    Display.people(@people)
   end
 
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [enter the number]: '
     person_type = gets.chomp
-    # Replaced the if-else block for creating a person with a case statement for better readability.
     case person_type
-    when '1' then create_person_by_type(STUDENT)
-    when '2' then create_person_by_type(TEACHER)
+    when '1' then @people << PersonCreator.create_student
+    when '2' then @people << PersonCreator.create_teacher
     else puts 'Person type not recognized. Back to the main menu.'
     end
   end
 
-  # a method is added to create a person based on their type
-  def create_person_by_type(type)
-    print 'Name: '
-    name = gets.chomp
-    print 'Age: '
-    age = gets.chomp.to_i
-
-    case type
-    when STUDENT then create_student(name, age)
-    when TEACHER then create_teacher(name, age)
-    end
-  end
-
-  def create_student(name, age)
-    print 'Has parent permission? [Y/N]: '
-    # Simplified the assignment of parent_permission as a boolean.
-    parent_permission = gets.chomp.downcase == 'y'
-    person = Student.new(age, nil, name: name, parent_permission: parent_permission)
-    @people << person
-    puts 'Student created successfully'
-  end
-
-  def create_teacher(name, age)
-    print 'Specialization: '
-    specialization = gets.chomp
-    person = Teacher.new(age, specialization, name: name)
-    @people << person
-    puts 'Teacher created successfully'
-  end
-
   def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
-    @books << book
+    @books << BookCreator.create_book
     puts 'Book created successfully'
   end
 
   def create_rental
-    puts 'Select a book from the following list by number'
-    @books.each_with_index do |book, index|
-      puts "#{index}) Title: '#{book.title}', Author: #{book.author}"
-    end
-    book_index = gets.chomp.to_i
-    puts 'Select a person from the following list by number (not ID)'
-    @people.each_with_index do |person, index|
-      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    person_index = gets.chomp.to_i
-    print 'Date: '
-    date = gets.chomp
-    rental = Rental.new(date, @books[book_index], @people[person_index])
-    @rentals << rental
+    @rentals << RentalCreator.create_rental(@books, @people)
     puts 'Rental created successfully'
   end
 
   def display_rentals
     print 'ID of person: '
     person_id = gets.chomp.to_i
-    @rentals.each do |rental|
-      if rental.person.id == person_id
-        puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
-      end
-    end
+    Display.rentals(@rentals, person_id)
   end
 end
